a47c77b1ae693de19f852ba940a2912c9e7b0700
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-01 13:21:58 -0300

    Commit de teste de conexão com o GitHyb

--- Aula_01/Aula_01.R
+++ Aula_01/Aula_01.R
@@ -1,2 +1,20 @@
 #Aula 01 - Introdução
 
+2+5
+4-3
+4/2
+2*9
+conta = 2+5
+conta
+
+conta <- 9-(2+5)
+conta
+
+#Maneira errada
+sequencia <- 1,2,3,4,5
+
+sequencia <- c(1,2,3,4,5)
+sequencia
+
+sequencia <- seq(1,1000)
+View(sequencia)
