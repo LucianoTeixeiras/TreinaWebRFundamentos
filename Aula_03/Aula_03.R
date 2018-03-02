062ca31bc982e339bc1986046e1923805994ec86
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-03-01 22:41:48 -0300

    Commit Aula 03

--- Aula_03/Aula_03.R
+++ Aula_03/Aula_03.R
@@ -6,3 +6,23 @@
 3-1
 4/2
 3*3
+
+#Criando um objeo
+conta = 2+3
+conta
+
+#ou
+conta1 <- 2+6
+conta1
+
+#Criando uma sequência numérica
+sequencia <- seq(1,1000,by = 10)
+sequencia
+
+#Comandos Básicos
+mean(sequencia)
+median(sequencia)
+min(sequencia)
+max(sequencia)
+
+summary(sequencia)
