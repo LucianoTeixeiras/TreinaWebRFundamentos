b5c685674b457ab61668c993349e3405f42e2a01
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-04 11:43:56 -0300

    Aula 05 - Matrizes e quadro de dados (Dataframes)

--- Aula_04/Aula_04.R
+++ Aula_04/Aula_04.R
@@ -7,3 +7,35 @@ nome
 
 #Para adicionar outros caracteres em um outro objeto, é necessário a 
 #utilização de aspas.
+
+nomes <- c("Vinicius","João","Maria")
+nomes
+
+#Vetor é uma coleção de objetos
+a<-c(1,2,3,4,5,6)
+b<-c(6,7,8,9)
+c<-c(a,b)
+c
+dist(c)
+unique(c)
+
+#Os vetores podem ter números e objetos
+
+vetor<-c("a",1,"b",2)
+vetor
+
+#Gerando números aleatórios
+sample(x=1:50)
+
+sample(x=1:10, size = 5)
+amostra <- sample(x=1:50, size = 10, replace = T)
+amostra
+
+#Indexação
+
+amostra[2]
+amostra[10]
+
+amostra[7:8]
+
+summary(amostra)
---
+++ Aula_05/Aula_05.R
@@ -0,0 +1,2 @@
+#Aula 05 - Matrizes e quadro de dados (Dataframes)
+
