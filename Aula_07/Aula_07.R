c0bd278c24a3f2f66e4eacc59291cdacdc90c2ed
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-04 16:31:17 -0300

    AUla 07 - Análise de Variância

--- Aula_07/Aula_07.R
+++ Aula_07/Aula_07.R
@@ -25,3 +25,32 @@ summary(tabela)
 
 attach(tabela)
 
+#Aula 07 - Regressões Lineares
+
+tabela
+dados <-tabela
+
+dados
+View(dados)
+
+modelo <-lm(tamanho_cm~adubo_gr)
+
+summary(dados)
+
+anova(modelo)
+summary(modelo)
+
+#Aula 07 - Regressões Lineares - Parte 2 - Gráficos de regressão liner
+
+#Fazendo gráfico com o comando polt
+
+plot(tamanho_cm~adubo_gr)
+
+curve(-2.88133+0.72110*x,add = T)
+
+plot(tamanho_cm~adubo_gr,xlab = 'Quantidade de Adubo em (Kg)', ylab = 'Crescimento da Planta em (Cm)', pch = 16)
+
+curve(-2.88133+0.72110*x,add = T, lty = 2)
+
+#Aula 07 -  Parte 01 - Análise de variância
+
