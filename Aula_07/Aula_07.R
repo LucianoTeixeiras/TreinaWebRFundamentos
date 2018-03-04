1e41ceef45f8d9c92604e8ffb5f5905c8ec45649
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-04 16:39:04 -0300

    Commit aula 07 analise de variancia
    Commit aula 07 analise de variancia

--- Aula_07/Aula_07.R
+++ Aula_07/Aula_07.R
@@ -54,3 +54,22 @@ curve(-2.88133+0.72110*x,add = T, lty = 2)
 
 #Aula 07 -  Parte 01 - Análise de variância
 
+library(readr)
+tabela2 <- read_delim("tabelaMarca.csv", ";", escape_double = FALSE, 
+                     trim_ws = TRUE)
+View(tabela2)
+
+summary(tabela2)
+
+attach(tabela2)
+
+#Definição do problema/pergunta
+
+#H1: A marca do adubo altera o crescimento da planta?
+#H0: Não existe diferença entre as marcas de adubo
+
+modeloaov <- aov(tamanho_cm~adubo_marca)
+
+summary(modeloaov)
+
+summary(tabela2)
