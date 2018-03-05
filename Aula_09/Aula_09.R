56303d2acc7a9b534de9fe8a17bc5ae04334c006
LucianoTeixeiras <luciano.teixeiras@gmail.com>
2018-03-04 22:38:01 -0300

    Commit final

--- Aula_09/Aula_09.R
+++ Aula_09/Aula_09.R
@@ -1,1 +1,22 @@
 #Aula 09 - Analisando dados do Facebook no R
+
+library(readr)
+facebookData <- read_delim("facebookData.csv", 
+                           ";", escape_double = FALSE, trim_ws = TRUE)
+
+dados<-read.csv("facebookData.csv", h=T)
+
+attach(facebookData)
+
+View(facebookData)
+
+
+
+head(facebookData)
+
+library("rJava")
+library("qdap")
+
+plot(shares.count~id)
+
+text(id, shares.count, labels = id, cex = 1, pos = 3)
